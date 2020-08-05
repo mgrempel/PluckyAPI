efa300a2e74cb9d85b28d907f03dcb16dfdf4ab5
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-08-05 18:24:13 -0500

    Updated Select with new query struct strategy

--- Builders/selectbuilder.go
+++ Builders/selectbuilder.go
@@ -1,17 +1,22 @@
 package builders
 
 import (
+	"PluckyAPI/Models"
 	"fmt"
 )
 
 //Select Selects all data from a specified table in the database. Only handling select all for now.
-func (bld Builder) Select(table string) (selectStatment string, err error) {
+func (bld Builder) Select(table string, query models.Query) (queryWithSelect models.Query, err error) {
 	//Check to see if table exists in db before proceeding
 	if bld.Tables[table] == nil {
-		return "", fmt.Errorf("table does not exist in the database")
+		return models.Query{}, fmt.Errorf("table does not exist in the database")
 	}
 
-	return fmt.Sprintf("SELECT * FROM %s", table), nil
+	//No params added here because SQL server is a little oaf.
+	selectStatement := fmt.Sprintf("SELECT * FROM %s", table)
+	query.AppendToQuery(selectStatement)
+
+	return query, nil
 
 	// //Prepare the Query
 	// preparedQuery, err := db.Prepare(query)
