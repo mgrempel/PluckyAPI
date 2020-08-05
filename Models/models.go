9dc4da0ce4647314b73273c18f17389e739b3549
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-08-05 18:10:39 -0500

    Added query struct and supporting methods

--- Models/models.go
+++ Models/models.go
@@ -10,3 +10,31 @@ type Column struct {
 	ColumnName string
 	ColumnType string
 }
+
+//------------------------------------------------------------------------------
+type Query struct {
+	queryString string
+	parameters  []string
+}
+
+//AddParameter Adds a new parameter to the query for binding later on
+func (q *Query) AddParameter(newParam string) {
+	q.parameters = append(q.parameters, newParam)
+}
+
+//AppendToQuery Adds a new clause (or part of one) to the query.
+func (q *Query) AppendToQuery(querySection string) {
+	q.queryString += querySection
+}
+
+//GetQuery returns the query
+func (q Query) GetQuery() string {
+	return q.queryString
+}
+
+//GetParams returns the query parameters.
+func (q Query) GetParams() []string {
+	return q.parameters
+}
+
+//------------------------------------------------------------------------------
