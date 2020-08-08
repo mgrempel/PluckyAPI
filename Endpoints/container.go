1165d91a814555a158320b9c7b9b93d4d1e1ad38
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-08-07 22:57:30 -0500

    Updated container struct, uses builder object to parse queries now.

--- Endpoints/container.go
+++ Endpoints/container.go
@@ -1,9 +1,13 @@
 package endpoints
 
-import "database/sql"
+import (
+	"PluckyAPI/Builders"
+	"database/sql"
+)
 
 //Container Environment container struct used for dependency injection
 //Any general utility such as a logger should have a field
 type Container struct {
-	Db *sql.DB
+	Db      *sql.DB
+	Builder builders.Builder
 }
