d198dbbbbb4d970e4da5709c2eaaeea9ed350605
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-08-08 17:22:44 -0500

    Added findColumns for ensuring a request contains valid columns

--- Builders/builder.go
+++ Builders/builder.go
@@ -18,3 +18,14 @@ func (bld Builder) findTable(table string) (check bool) {
 
 	return misc.Contains(tableNames, table)
 }
+
+func (bld Builder) findColumns(tableName string, columns []string) bool {
+	tableColumns := bld.Tables[tableName].Columns
+
+	for _, columnName := range tableColumns {
+		if !misc.Contains(columns, columnName.ColumnName) {
+			return false
+		}
+	}
+	return true
+}
