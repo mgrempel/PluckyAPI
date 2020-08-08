c461f72da1527dbbf1050f19b4c81462a4f90ab5
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-08-07 22:56:32 -0500

    Updated table check in select builder, fixed screwup in builder map type

--- Builders/builder.go
+++ Builders/builder.go
@@ -2,9 +2,19 @@ package builders
 
 import (
 	"PluckyAPI/Models"
+	"PluckyAPI/Utilities/misc"
 )
 
 //Builder struct contains all required functions for parsing a request into an SQL statement
 type Builder struct {
-	Tables map[string][]models.Table
+	Tables map[string]models.Table
+}
+
+func (bld Builder) findTable(table string) (check bool) {
+	var tableNames = make([]string, 0)
+	for key := range bld.Tables {
+		tableNames = append(tableNames, key)
+	}
+
+	return misc.Contains(tableNames, table)
 }
